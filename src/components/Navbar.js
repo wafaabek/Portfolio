290c345a565ff733d0e20ddbc9874e982b83b6a0
wafaabek <wafaabek3@gmail.com>
2024-08-25 17:48:37 +0100

    second_commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
+
 import {
   AiFillStar,
   AiOutlineHome,
@@ -95,7 +95,7 @@ function NavBar() {
           
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/wafaabek/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
